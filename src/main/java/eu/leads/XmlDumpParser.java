339be5a4c7fc358e13a6b4cec56dbc682463182c
Dorian Burihabwa <dorian@burihabwa.com>
2020-01-14 12:06:02 +0100

    Remove Infinispan code and update dependencies

--- src/main/java/eu/leads/XmlDumpParser.java
+++ src/main/java/eu/leads/XmlDumpParser.java
@@ -1,54 +1,37 @@
 package eu.leads;
 
-import org.apache.log4j.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.sweble.wikitext.dumpreader.DumpReader;
-import org.sweble.wikitext.dumpreader.export_0_8.PageType;
-import org.sweble.wikitext.dumpreader.export_0_8.RevisionType;
+import org.sweble.wikitext.dumpreader.export_0_10.PageType;
 
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.nio.charset.Charset;
 import java.util.List;
 
 public class XmlDumpParser {
 
-	static Logger logger = Logger.getLogger(XmlDumpParser.class.getClass());
+    static Logger logger = LoggerFactory.getLogger(XmlDumpParser.class.getClass());
 
-	/**
-	 * @param args
-	 * @throws Exception
-	 */
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws Exception {
 
-		final DumbInfinispanClient ispnClient = new DumbInfinispanClient();
-
-		if (System.getProperty("ISPN_CLIENT") != null) {
-			String xmlFileName = args[0];
-			final File file = new File(xmlFileName);
-			logger.info("Start parsing dump file "+xmlFileName);
-			DumpReader dr = new DumpReader(file, logger) {
-				@Override
-				protected void processPage(Object mediaWiki, Object page) {
-					PageType p = (PageType) page;
-					List<Object> items = p.getRevisionOrUpload();
-					logger.info("nbRevisions: " + items.size());
-					for (Object v : items) {
-						ispnClient.insertPageVersion(p, ((RevisionType)v).getText().getValue());
-//						ispnClient.insertPageVersion(p, v);
-					}
-				}
-			};
-			dr.unmarshal();
-
-			int storedKeys = ispnClient.countKeys();
-			logger.info("Total number of stored keys at the end of parsing: "+storedKeys);
-			
-		}
-		else {
-			logger.info("Waiting for other nodes...");
-			while (true){
-				Thread.sleep(1000);
-				logger.info("Current number of keys in cache: "+ ispnClient.countKeys());
-
-			}
-		}
-	}
+        String xmlFileName = args[0];
+        final File file = new File(xmlFileName);
+        final InputStream is = new FileInputStream(file);
+        logger.info("Start parsing dump file " + xmlFileName);
+        DumpReader reader = new DumpReader(is, Charset.forName("UTF-8"), "", logger, false) {
+            @Override
+            protected void processPage(Object mediaWiki, Object page) {
+                PageType p = (PageType) page;
+                List<Object> revisions = p.getRevisionOrUpload();
+                logger.info("nbRevisions: " + revisions.size());
+                for (Object v : revisions) {
+                    System.out.println(v);
+                }
+            }
+        };
+        reader.unmarshal();
+    }
 }
